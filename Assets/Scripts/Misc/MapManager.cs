24a4eae6184e293d52e3e6d59fab3bc8ded6e326
Felipe Theil <lipetheil18@gmail.com>
2023-11-30 21:30:39 -0300

    Sistema de mapa

--- Assets/Scripts/Misc/MapManager.cs
+++ Assets/Scripts/Misc/MapManager.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
+    public GameObject mapMenu;
     public GameObject tilemap;
+    public Image mapImage;
+    public Image playerImage;
+
+    private Vector3 tilemapSize;
+    private float coefficientX, coefficientY;
 
     // Start is called before the first frame update
     void Start()
     {
-        var b = tilemap.GetComponent<Renderer>().bounds;
+        tilemapSize = tilemap.GetComponent<Renderer>().bounds.size;
 
+        coefficientX = tilemapSize.x / mapImage.rectTransform.rect.width;
+        coefficientY = tilemapSize.y / mapImage.rectTransform.rect.height;
+
+        /*
         Debug.Log("Scene Center: " + b.center);
         Debug.Log("Scene Extents X: " + b.extents.x);
         Debug.Log("Scene Extents Y: " + b.extents.y);
@@ -18,11 +29,19 @@ public class MapManager : MonoBehaviour
         Debug.Log("Scene maxX: " + b.max.x);
         Debug.Log("Scene minY: " + b.min.y);
         Debug.Log("Scene maxY: " + b.max.y);
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M)) {
+            if (!mapMenu.activeSelf)
+                mapMenu.SetActive(true);
+            else
+                mapMenu.SetActive(false);
+        }
+
+        playerImage.rectTransform.anchoredPosition = new Vector3(GameManager.instance.GetPlayer().transform.position.x / coefficientX, GameManager.instance.GetPlayer().transform.position.y / coefficientY, 0);
     }
 }
